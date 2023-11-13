8d546712063d347281990de3cf4106b0b3dac401
Cometia0 <cometia0@sjtu.edu.cn>
2023-11-14 00:21:40 +0800

    Update firmware.sh

--- firmware.sh
+++ firmware.sh
@@ -768,6 +768,7 @@ USB/SD devices are connected. "
 
 	usb_device="${usb_devs[${usb_dev_index}-1]}"
 	mkdir /tmp/usb > /dev/null 2>&1
+	echo {$usb_device}
 	if ! sudo mount "${usb_device}" /tmp/usb > /dev/null 2>&1; then
 		if ! sudo mount "${usb_device}1" /tmp/usb > /dev/null 2>&1; then
 			backup_fail "USB backup device failed to mount; cannot proceed."
